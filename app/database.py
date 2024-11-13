dae3f35522fdc42b73233734728db792237a26ce
c1trus <citrxs@ya.ru>
2024-11-13 20:22:40 +0300

    добавлен класс для работы с JSON базой

--- app/database.py
+++ app/database.py
@@ -1,3 +1,4 @@
+import json
 import sqlite3
 
 
@@ -13,3 +14,45 @@ class Database:
 
     def __del__(self):
         self.connection.close()
+
+
+class JsonDatabase:
+
+    def __init__(self):
+        with open("computers.json", "r", encoding="utf-8") as f:
+            self.data = json.load(f)
+
+    def add_computer(self, user, **kwargs):
+        """
+
+        :param user: айди пользователя в базе данных
+        :param kwargs:
+        cpu - процессор
+        cpu_cooler - кулер для процессора
+        gpu - видеокарта
+        mb - материнская плата
+        ram - оперативная память
+        disk - устройство памяти
+        power - блок питания
+        core - корпус
+        """
+
+        new_id = max(map(int, self.data.keys())) + 1
+
+        computer = {
+            "user": user,
+            "cpu": kwargs.get("cpu"),
+            "cpu_cooler": kwargs.get("cpu_cooler"),
+            "gpu": kwargs.get("gpu"),
+            "mb": kwargs.get("mb"),
+            "ram": kwargs.get("ram"),
+            "disk": kwargs.get("disk"),
+            "power": kwargs.get("power"),
+            "core": kwargs.get("core")
+        }
+
+        self.data[new_id] = computer
+
+        with open("computers.json", "w", encoding="utf-8") as f:
+            json.dump(self.data, f, indent=4, ensure_ascii=False)
+
